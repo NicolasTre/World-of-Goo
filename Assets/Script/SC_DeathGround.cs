71e65e9cc60893a6429bda510f4b163926873716
NicolasTre <N.TREZEGUET@lahorde.school>
2024-10-15 17:28:30 +0200

    add respawn for blob and add a death for the struct

--- Assets/Script/SC_DeathGround.cs
+++ Assets/Script/SC_DeathGround.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class SC_DeathGround : MonoBehaviour
 {
     [SerializeField] private string sceneName;
+    [SerializeField] private GameObject blobSpawnPoint;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -11,5 +12,10 @@ public class SC_DeathGround : MonoBehaviour
         {
             SceneManager.LoadScene(sceneName);
         }
+        else if (collision.gameObject.tag == "BlobToUse")
+        {
+            collision.gameObject.transform.position = blobSpawnPoint.transform.position;
+        }
+
     }
 }
